85d6b4c52ca751191a874fc3d014279bf0d5dcce
Safak <akdemirsafak>
2022-04-15 23:11:09 +0300

    addedSomePackages

--- Store_Dapper/Program.cs
+++ Store_Dapper/Program.cs
@@ -1,3 +1,9 @@
+using System.Data;
+using System.Reflection;
+using MediatR;
+using Npgsql;
+using Store_Dapper.Repositores;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,37 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+//DI Container
+// builder.Services.AddScoped<NotFoundProductFilter>();
+
+
+
+builder.Services.AddScoped<IDbConnection>(sp => new NpgsqlConnection(builder.Configuration.GetConnectionString("Postgresql")));
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<IBookRepository, BookRepository>();
+builder.Services.AddScoped<UnitOfWork>();
+builder.Services.AddScoped<IDbTransaction>(sp =>
+{
+
+    var connection = sp.GetRequiredService<IDbConnection>();
+    connection.Open();
+    return connection.BeginTransaction();
+
+
+});
+
+
+
+builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+
+
+
+
 
 var app = builder.Build();
 
